c0c0abf52c0e5b96461b079cb50846013b3c7ded
taliapipa <125282306+taliapipa@users.noreply.github.com>
2023-07-31 09:53:21 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let count = 0;
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function(e){
+        const style = e.currentTarget.classList;
+        if(style.contains('decrease')){
+            count--;
+        } else if (style.contains('increase')){
+            count++;
+        }else if (style.contains('reset')){
+            count = 0;
+        }
+        value.textContent = count;
+        
+    });
+});
